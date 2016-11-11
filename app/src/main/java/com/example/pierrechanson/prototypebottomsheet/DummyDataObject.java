058223f8ae2cf6229ee4366e2e70b44119f12a79
Guillaume Meral <guillaume@2denker.de>
2016-11-11 15:05:59 +0100

    add DummyDataObject

---
+++ app/src/main/java/com/example/pierrechanson/prototypebottomsheet/DummyDataObject.java
@@ -0,0 +1,17 @@
+package com.example.pierrechanson.prototypebottomsheet;
+
+/**
+ * Created by guillaumemeral on 11/11/16.
+ */
+
+public class DummyDataObject {
+    public String title;
+    public String availableBikes;
+    public String distanceTime;
+
+    public DummyDataObject(String title, String availableBikes, String distanceTime) {
+        this.title = title;
+        this.availableBikes = availableBikes;
+        this.distanceTime = distanceTime;
+    }
+}
